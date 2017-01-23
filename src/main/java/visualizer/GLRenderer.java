f5ba2130a07d1ef799cececb65fdd5ca8e7fa5d5
serious2monkeys <doronantonin@gmail.com>
2017-01-23 21:52:07 +0500

    rendering sample

--- src/main/java/visualizer/GLRenderer.java
+++ src/main/java/visualizer/GLRenderer.java
@@ -2,11 +2,30 @@ package visualizer;
 
 
 import com.jogamp.opengl.*;
+import eu.mihosoft.vrl.v3d.CSG;
+import eu.mihosoft.vrl.v3d.Polygon;
+import eu.mihosoft.vrl.v3d.Vector3d;
+
+import java.nio.FloatBuffer;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class GLRenderer implements GLEventListener {
 
-    public GLRenderer() {
+    private CSG scene;
 
+    private Vector3d rotate = new Vector3d(0, 0, 0);
+
+    public GLRenderer() {
+        scene = null;
+    }
+
+    public GLRenderer(CSG scene) {
+        this.scene = scene;
+    }
+
+    public void setRotate(Vector3d rotate) {
+        this.rotate = rotate;
     }
 
     @Override
@@ -18,6 +37,11 @@ public class GLRenderer implements GLEventListener {
         gl.glClearColor(1.0f, 1.0f, 1.0f, 0f);
         gl.glShadeModel(GL2.GL_SMOOTH);
         gl.glClearDepth(1.0f);
+        gl.glLightfv(gl.GL_LIGHT0, gl.GL_POSITION, FloatBuffer.wrap(new float[]{1f, 1f, 1f, 0}));
+        gl.glMaterialfv(gl.GL_FRONT, gl.GL_SPECULAR, FloatBuffer.wrap(new float[]{1.0f, 1.0f, 1.0f, 1.0f}));
+        gl.glMaterialfv(gl.GL_FRONT, gl.GL_SHININESS, FloatBuffer.wrap(new float[]{50f}));
+        gl.glEnable(gl.GL_LIGHTING);
+        gl.glEnable(gl.GL_LIGHT0);
         gl.glEnable(GL.GL_DEPTH_TEST);
         gl.glDepthFunc(GL.GL_LEQUAL);
         gl.glHint(GL2.GL_PERSPECTIVE_CORRECTION_HINT, GL.GL_NICEST);
@@ -40,7 +64,7 @@ public class GLRenderer implements GLEventListener {
         //GLU glu = new GLU();
         gl.glViewport(0, 0, width, height);
         gl.glMatrixMode(GL2.GL_PROJECTION);
-        gl.glLoadIdentity();
+        //gl.glLoadIdentity();
 
         if (height <= 0) { // avoid a divide by zero error!
 
@@ -60,16 +84,40 @@ public class GLRenderer implements GLEventListener {
         GL2 gl = drawable.getGL().getGL2();
         gl.glHint(GL2.GL_POINT_SMOOTH_HINT, GL.GL_NICEST);
         gl.glEnable(GL2.GL_POINT_SMOOTH);
-        gl.glPointSize(3f);
+        gl.glPointSize(1.5f);
         gl.glClearColor(1.0f, 1.0f, 1.0f, 0f);
         // Clear the drawing area
         gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
         // Reset the current matrix to the "identity"
         gl.glPushMatrix();
-        gl.glLoadIdentity();
+/*        gl.glLoadIdentity();*/
         // Move the "drawing cursor" around
-        
-        
+
+        float[] matrix = new float[16];
+        gl.glGetFloatv(gl.GL_MODELVIEW_MATRIX, FloatBuffer.wrap(matrix));
+        gl.glLoadIdentity();
+        gl.glRotated(rotate.z, rotate.x, rotate.y, 0.0);
+        gl.glMultMatrixf(FloatBuffer.wrap(matrix));
+
+        if (scene != null && !scene.getPolygons().isEmpty()) {
+            gl.glBegin(GL.GL_TRIANGLES);
+            gl.glColor3d(1, 0, 0);
+            gl.glLineWidth(1.5f);
+            List<Polygon> polygons = scene.getPolygons();
+            Vector3d maxBounds = scene.getBounds().getBounds();
+            double bound = Stream.of(maxBounds.x, maxBounds.y, maxBounds.z).max(Double::compareTo).get();
+            polygons.forEach(polygon -> polygon.vertices.forEach(vertex ->
+                    gl.glVertex3d(vertex.pos.x / bound, vertex.pos.y / bound, vertex.pos.z / bound)
+            ));
+            gl.glEnd();
+
+            gl.glColor3d(0, 0, 0);
+            gl.glBegin(GL.GL_POINTS);
+            polygons.forEach(polygon -> polygon.vertices.forEach(vertex ->
+                    gl.glVertex3d(vertex.pos.x / bound, vertex.pos.y / bound, vertex.pos.z / bound)
+            ));
+            gl.glEnd();
+        }
         /*
          gl.glEnable(GL.GL_LINE_SMOOTH);
             gl.glShadeModel(GL.GL_SMOOTH);
--- src/main/java/visualizer/VisualizerCL.java
+++ src/main/java/visualizer/VisualizerCL.java
@@ -4,13 +4,20 @@ import com.jogamp.opengl.awt.GLJPanel;
 import com.jogamp.opengl.util.FPSAnimator;
 import eu.mihosoft.vrl.v3d.CSG;
 import eu.mihosoft.vrl.v3d.STL;
+import eu.mihosoft.vrl.v3d.Vector3d;
 
 import javax.swing.*;
+import javax.swing.event.MouseInputAdapter;
+import javax.vecmath.Vector2d;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static visualizer.VisualizerCL.VisualizerMode.POLYGONAL;
+import static visualizer.VisualizerCL.VisualizerMode.REYCAST_JAVA;
 /*
  * Created by JFormDesigner on Sun Nov 27 18:32:31 YEKT 2016
  */
@@ -23,7 +30,55 @@ public class VisualizerCL extends JFrame {
 
     private FPSAnimator animator;
     private GLRenderer renderer;
-    CSG scene;
+    private CSG scene;
+    private VisualizerMode visualizerMode = POLYGONAL;
+    private int branchSize = 4;
+
+    private boolean mousePressed = false;
+    private Vector2d prevMousePos = new Vector2d(0.0, 0.0);
+    private Vector3d rotVector = new Vector3d(0, 0, 0);
+
+    MouseAdapter adapter = new MouseInputAdapter() {
+        @Override
+        public void mousePressed(MouseEvent e) {
+            mousePressed = true;
+            prevMousePos = new Vector2d(e.getX(), e.getY());
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            // mousePressed = false;
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            if (mousePressed) {
+                int dx = (int) (prevMousePos.x - e.getX());
+                int dy = (int) (prevMousePos.y - e.getY());
+                rotVector = new Vector3d(dy, dx, 0);
+                rotVector.z = Math.sqrt(dx * dx + dy * dy) / 3;
+
+                panel2.removeGLEventListener(renderer);
+                renderer = visualizerMode.equals(POLYGONAL)
+                        ? new GLRenderer(scene)
+                        : new GLRenderer();
+                renderer.setRotate(rotVector);
+                renderer.init(panel2);
+                panel2.addGLEventListener(renderer);
+                panel2.setAnimator(animator);
+                animator.start();
+
+                prevMousePos = new Vector2d(e.getX(), e.getY());
+                mousePressed = false;
+            }
+        }
+    };
+
+    enum VisualizerMode {
+        POLYGONAL,
+        REYCAST_JAVA
+    }
+
   /*
     // Generated using JFormDesigner Evaluation license - Anton Doronin
     private JPanel panel1;
@@ -36,9 +91,6 @@ public class VisualizerCL extends JFrame {
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
-                // Run this on another thread than the AWT event queue to
-                // make sure the call to Animator.stop() completes before
-                // exiting
                 new Thread(() -> {
                     animator.stop();
                     System.exit(0);
@@ -107,6 +159,16 @@ public class VisualizerCL extends JFrame {
         fileChooser.setVisible(false);
         if (fileChooser.getSelectedFile() != null) {
             scene = STL.file(fileChooser.getSelectedFile().toPath());
+            scene.setOptType(CSG.OptType.POLYGON_BOUND);
+            panel2.removeGLEventListener(renderer);
+            List<Vector3d[][]> rays = splitRays(scene);
+            renderer = visualizerMode.equals(POLYGONAL)
+                    ? new GLRenderer(scene)
+                    : new GLRenderer();
+            renderer.init(panel2);
+            panel2.addGLEventListener(renderer);
+            panel2.setAnimator(animator);
+            animator.start();
         }
     }
 
@@ -121,14 +183,22 @@ public class VisualizerCL extends JFrame {
         menuItem3 = new JMenuItem();
         panel1 = new JPanel();
         panel2 = new GLJPanel();
-
+        renderer = new GLRenderer();
         animator = new FPSAnimator(panel2, 60, true);
+        panel2.setForeground(Color.lightGray);
+        panel2.addGLEventListener(renderer);
+        panel2.setAnimator(animator);
+        animator.start();
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         //======== this ========
         Container contentPane = getContentPane();
         contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.LINE_AXIS));
 
+        {
+            panel2.addMouseListener(adapter);
+            panel2.addMouseMotionListener(adapter);
+        }
         //======== menuBar1 ========
         {
 
@@ -155,10 +225,12 @@ public class VisualizerCL extends JFrame {
 
                 //---- menuItem2 ----
                 menuItem2.setText("\u0413\u0435\u043d\u0435\u0440\u0430\u0446\u0438\u044f \u043f\u043e\u0432\u0435\u0440\u0445\u043d\u043e\u0441\u0442\u0438");
+                menuItem2.addActionListener(e -> visualizerMode = POLYGONAL);
                 menu2.add(menuItem2);
 
                 //---- menuItem3 ----
                 menuItem3.setText("\u041f\u0430\u0440\u0430\u043b\u043b\u0435\u043b\u044c\u043d\u044b\u0439 \u0440\u0435\u0439\u043a\u0430\u0441\u0442\u0438\u043d\u0433");
+                menuItem3.addActionListener(e -> visualizerMode = REYCAST_JAVA);
                 menu2.add(menuItem3);
             }
             menuBar1.add(menu2);
@@ -194,7 +266,41 @@ public class VisualizerCL extends JFrame {
         contentPane.add(panel1);
         pack();
         setLocationRelativeTo(getOwner());
-        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    private List<Vector3d[][]> splitRays(CSG geometry) {
+        List<Vector3d[][]> rays = new ArrayList<>();
+        int pixelNum = panel2.getWidth();
+        branchSize = (int) Math.sqrt(pixelNum);
+        Vector3d bound = geometry.getBounds().getMax();
+        double maxDimension = Stream.of(bound.x, bound.y, bound.z)
+                .max(Double::compareTo).get();
+        double step = (maxDimension / pixelNum) * 2;
+        for (int pos = 0; pos <= pixelNum; pos += branchSize) {
+            Vector3d[][] coherentRays = new Vector3d[branchSize][branchSize];
+            double delta = -maxDimension + pos * step;
+            for (int i = 0; i < branchSize; i++) {
+                for (int j = 0; j < branchSize; j++) {
+                    coherentRays[i][j] = new Vector3d(delta + i * step, delta + j * step, Double.POSITIVE_INFINITY);
+                }
+            }
+            rays.add(coherentRays);
+        }
+        return rays;
+    }
+
+    @Override
+    public void setVisible(boolean show) {
+        if (!show) {
+            if (animator != null) {
+                animator.stop();
+            }
+        }
+        super.setVisible(show);
+        if (!show)
+            if (animator != null) {
+                animator.start();
+            }
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
